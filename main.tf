ecd470314965353c516871ce17463fdd1b73dbaa
idrissfofana <98565785+idrissfofana@users.noreply.github.com>
2022-05-23 21:56:46 -0400

    Update main.tf
    added another resource

--- main.tf
+++ main.tf
@@ -1,6 +1,9 @@
 resource "aws_ecs_cluster" "cluster" {
   name = "idriss-ecs-cluster"
 
+  resource "aws_ecs_cluster_capacity_providers" "cluster" {
+  cluster_name = aws_ecs_cluster.cluster.name
+
   capacity_providers = ["FARGATE_SPOT", "FARGATE"]
 
   default_capacity_provider_strategy {
